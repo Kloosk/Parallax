d35757cfa77825b215ab960392a1782f7428fb51
Kloosk <lukasz.gras.97@gmail.com>
2021-02-11 22:21:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { ThemeProvider, createGlobalStyle} from 'styled-components';
 
+const theme = {
+    white: "#D9D9D9",
+    red: "#8C030E",
+    dark: "#A65E44",
+    light: "#BF907E",
+    black: "#03060D"
+};
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 function App() {
   return (
-    <>
-    </>
+      <ThemeProvider theme={theme}>
+          <GlobalStyle/>
+
+      </ThemeProvider>
   );
 }
 
---
+++ src/components/main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "./style";
+
+const Index = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+};
+
+export default Index;
